1534532b94a567ec7b03e42b47e1c1af98502e37
Netto <nettofarah@gmail.com>
2011-11-13 17:10:33 -0200

    adding daemon to run server in prod (minor fix)

--- server_daemon.rb
+++ server_daemon.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'daemons'
 
 pwd = Dir.pwd
-Daemons.run_proc('notification-bot-server', {:dir_mode => :normal, :dir => "/opt/pids/notification-bot-server.pid"}) do
+Daemons.run_proc('notification-bot-server', {:dir_mode => :normal, :dir => "/opt/pids/sinatra"}) do
   Dir.chdir(pwd)
   exec "RACK_ENV=production ruby server.rb -p 5678"
 end
