20403579674b7c114b5f60d9b8c615c40688fda8
Netto <nettofarah@gmail.com>
2011-11-13 17:08:56 -0200

    adding daemon to run server in prod

---
+++ server_daemon.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'daemons'
+
+pwd = Dir.pwd
+Daemons.run_proc('notification-bot-server', {:dir_mode => :normal, :dir => "/opt/pids/notification-bot-server.pid"}) do
+  Dir.chdir(pwd)
+  exec "RACK_ENV=production ruby server.rb -p 5678"
+end
